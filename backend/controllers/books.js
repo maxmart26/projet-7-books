401a0f40cbcc1c20fa82554e051766a19f09165a
maxmart26 <torgue.maxime@gmail.com>
2023-07-30 18:55:11 +0200

    version avec les ajout de note avec un autre utilisateur

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -64,6 +64,51 @@ exports.deleteBook = (req, res, next) => {
       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
       .catch(error => res.status(400).json({ error }));
   }
-exports.bestrating = (req,res) => {
-  
+
+exports.bestrating = async (req,res) => {
+  try {
+    const books = await Book.find().sort({averageRating:-1}).limit(3);
+  books.forEach((book) => {
+    book.imageUrl = "http://localhost:4000/images/" + book.imageUrl;
+  });
+  res.send(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("erreur:" + error.message);
+  }
+}
+
+exports.ratingBook = async (req,res) => {
+  const id = req.params.id;
+  if (id == null || id == "undefined") {
+    res.status(400).send("Book id is missing");
+    return;
+  }
+  const rating = req.body.rating;
+  const userId = req.body.userId;
+  try {
+    const book = await Book.findById(id);
+    if (book == null) {
+      res.status(404).send("Book not found");
+      return;
+    }
+    const ratingsInDb = book.ratings;
+    const previousRatingFromCurrentUser = ratingsInDb.find((rating) => rating.userId == userId);
+    if (previousRatingFromCurrentUser != null) {
+      res.status(400).send("You have already rated this book");
+      return;
+    }
+    const newRating = { userId, grade: rating };
+    ratingsInDb.push(newRating);
+    book.averageRating = calcul(ratingsInDb);
+    await book.save();
+    res.send("Rating posted");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Something went wrong:" + e.message);
+  }
+}
+function calcul(ratings){
+  const sum = ratings.reduce((acc, rating) => acc + rating.grade, 0);
+  return sum / ratings.length;
 }
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -7,9 +7,11 @@ const {upload} = require('../multer/multer');
 
 router.get('/', bookCtrl.getAllBook);
 router.post('/',auth, upload.single("image"), bookCtrl.createBook);
+router.get('/bestrating',bookCtrl.bestrating);
 router.get('/:id', bookCtrl.getBook);
+router.post('/:id/rating', bookCtrl.ratingBook);
 router.put('/:id',auth,upload.single("image"),bookCtrl.uptateOneBook);
 router.delete('/:id',auth, bookCtrl.deleteBook);
-router.get('/bestrating',bookCtrl.bestrating);
+
 
 module.exports = router
