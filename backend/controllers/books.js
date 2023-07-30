37925d6531007e31e46280ed983bcd644e236c2d
maxmart26 <torgue.maxime@gmail.com>
2023-07-30 15:38:41 +0200

    version avec la page id des books

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,12 +1,11 @@
 const {Book} = require('../models/Book');
-const {bookss} = require('../books');
 
 
 exports.createBook =  async (req, res,next) => {
-  const file = req.file;
+  const file = req.file.filename;
   const stringifiedBook = req.body.book;
   const book = JSON.parse(stringifiedBook);
-  book.imageUrl = file.path;
+  book.imageUrl = file;
   try{
   const result = await Book.create(book);
   res.send({ message:"livre crée"})
@@ -17,15 +16,34 @@ exports.createBook =  async (req, res,next) => {
   }
 }
 
-exports.getAllBook = (req, res,) => {
-    res.send(bookss);
-  };
+exports.getAllBook = async (req, res,) => {
+  try {
+    const books = await Book.find();
+    books.forEach((book) => {
+      book.imageUrl = "http://localhost:4000/images/" + book.imageUrl;
+    });
+    res.send(books);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("erreur:" + e.message);
+  }
+}
 
-exports.getBook = (req, res, next) => {
-    Books.findOne({ _id: req.params.id })
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  };
+exports.getBook = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const book = await Book.findById(id);
+    if (book == null) {
+      res.status(404).send("Book not found");
+      return;
+    }
+    book.imageUrl = "http://localhost:4000/images/" + book.imageUrl;
+    res.send(book);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Something went wrong:" + e.message);
+  }
+}
 
 exports.uptateOneBook = (req, res, next) => {
     Books.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
